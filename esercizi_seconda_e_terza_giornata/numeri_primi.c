b63a7163022c970540d6279669b9ad37b88d5af3
ok <ok>
2022-02-03 12:43:39 +0100

    le matrici le ha pushate Andreana

---
+++ esercizi_seconda_e_terza_giornata/numeri_primi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    x = 0;
+    int y;
+    y = 0;
+    while (x < 10)
+    {
+        while(y < 10)
+        {
+            printf("%i, %i\n",x , y);
+            y = y + 1;
+        }
+        x = x + 1;
+    }
+    return(0);
+}
