dc029b41c68aaeb663d19ede90149f90f8acf09d
andreana <andreana@ciao.com>
2022-02-03 12:39:50 +0100

    esercizi matrici

---
+++ for_loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int x=0;
+    for( ; x<= 10; x=x+1)
+{
+    printf("%i\n" , x);
+}
+printf("%i\n", x);
+return(0);
+}
---
+++ matrice.c
@@ -0,0 +1,111 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct canvas
+{
+	int x_size;
+	int y_size;
+	char **arr;
+} canvas_struct;
+
+canvas_struct *generate_new_canvas(int new_x_size, int new_y_size)
+{
+	canvas_struct *canvas = malloc(sizeof(canvas_struct));
+	canvas->x_size = new_x_size;
+	canvas->y_size = new_y_size;
+	canvas->arr = malloc(sizeof(char *) * new_y_size);
+	for (int i = 0; i < new_y_size; i++)
+	{
+		canvas->arr[i] = malloc(sizeof(char) * new_x_size);
+		for (int n = 0; n < new_x_size; n++)
+		{
+			canvas->arr[i][n] = '-';
+		}
+	}
+	return canvas;
+}
+
+void print_2d_array(canvas_struct *canvas)
+{
+	printf("\e[1;1H\e[2J"); // clear screen
+	printf(" ");
+	for (int n = 0; n < canvas->x_size; n++)
+	{
+		printf("%2d", n);
+	}
+	printf("\n");
+	for (int i = 0; i < canvas->y_size; i++)
+	{
+		printf("%2d", i);
+		for (int n = 0; n < canvas->x_size; n++)
+		{
+			printf("%c ", canvas->arr[i][n]);
+		}
+		printf("\n");
+	}
+}
+
+char get_pixel(int from_x, int from_y, canvas_struct *canvas)
+{
+	return canvas->arr[from_y][from_x];
+}
+
+void delete_pixel(int from_x, int from_y, canvas_struct *canvas)
+{
+	canvas->arr[from_y][from_x] = '-';
+}
+
+void put_pixel(int to_x, int to_y, canvas_struct *canvas)
+{
+	canvas->arr[to_y][to_x] = 'X';
+}
+
+void move_pixel(int from_x, int from_y, int to_x, int to_y, canvas_struct *canvas)
+{
+	char t;
+	t = get_pixel(from_x, from_y, canvas);
+	delete_pixel(from_x, from_y, canvas);
+	if (t == 'X')
+	{
+		put_pixel(to_x, to_y, canvas);
+	}
+	else
+	{
+		delete_pixel(to_x, to_y, canvas);
+	}
+}
+
+int main(int argc, char **argv)
+{
+	int size_x = 7;
+	int size_y = 8;
+	canvas_struct *canvas;
+
+	canvas = generate_new_canvas(size_x, size_y);
+
+    int x;
+    x= 0;
+    int y;
+    y=0;
+
+
+   while(x<size_x)
+    {
+        y = 0;
+        while(y<size_y)
+        {
+            put_pixel(x, y, canvas);
+            y=y+1;
+        }   
+        x= x+2;
+    }
+
+
+	print_2d_array(canvas);
+
+	return 0;
+}
+
+
+//Andreana_Coacci,Sofia_Celon,Lucrezia_Cerulli,Domenico_curatola,Galgano_briganti
