86fb68ac11c36d509521ed23041d2cfc0d55ecf8
Massimo <pazzo@sos.com>
2022-02-03 12:39:40 +0100

    esercizi_quarta_giornata

---
+++ esercizi_quarta_giornata/esercitazionefor.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{
+    for (int x; x<=10; x++)
+    {
+        printf ("%i\n", x);
+    }
+    return (0);
+}
+// Massimo Coen, Jemea Njie, Francesca Merlino, Ginevra Biasiotti, Martina Verna, Maria Cecilia Cattani
